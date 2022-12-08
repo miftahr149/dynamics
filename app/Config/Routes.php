311d49a153ff40f32860dd85105318e3c00ba882
GarySanderson9 <119645515+GarySanderson9@users.noreply.github.com>
2022-12-08 13:17:35 +0700

    Update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,12 +37,10 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 $routes->get('/about', 'About::index');
-<<<<<<< HEAD
 $routes->get('/homestay', 'Homestay::index');
-=======
 $routes->get('/competition', 'Competition::index');
 $routes->get('/contact', 'Contact::index');
->>>>>>> main
+
 
 /*
  * --------------------------------------------------------------------
