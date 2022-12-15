f29057554412905061419788ea7f67744c8b6254
Khoirul Mustofa <khoirul.mustofa2014@gmail.com>
2022-12-15 15:37:54 +0700

    Update MainLayout.php

---
+++ app/Views/MainLayout.php
@@ -0,0 +1,361 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7WT38KNK5Y"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-7WT38KNK5Y');
+    </script>
+    <meta charset="utf-8">
+    <title><?= SITE_NAME ?></title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Add site Favicon -->
+    <link rel="shortcut icon" href="<?= BASE_URL ?>assets/img/favicon.png" type="image/png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
+    <meta name="viewport" content="initial-scale = 1.0, user-scalable = no, width=device-width, height=device-height, maximum-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <link rel="amphtml" href="<?php echo current_url() ?>/amp">
+    <meta name="description" content="Dynamics 2023 merupakan ajang penguatan dan peningkatan jiwa kepemimpinan pelajar di-Indonesia khususnya di wilayah Jabodatabek, yang nantinya diharapkan dapat mengemban peran vital dilingkungan eksternal maupun internal" itemprop="description" />
+    <meta name="originalTitle" content="Nurul Fikri Leader League, Youth Charity Concert, dan Next Leader Competitions" />
+    <meta charset="utf-8">
+    <meta property="og:type" content="article" />
+    <meta property="og:site_name" content="dynamics_nfbs" />
+    <meta property="og:title" content="Nurul Fikri Leader League, Youth Charity Concert, dan Next Leader Competitions" />
+    <meta property="og:image" content="<?= BASE_URL ?>assets/img/thumbnail.jpg" />
+    <meta property="og:description" content="Dynamics 2023 merupakan ajang penguatan dan peningkatan jiwa kepemimpinan pelajar di-Indonesia khususnya di wilayah Jabodatabek, yang nantinya diharapkan dapat mengemban peran vital dilingkungan eksternal maupun internal" />
+    <meta property="og:url" content="<?php echo current_url() ?>" />
+    <meta property="fb:app_id" content="187960271237149" />
+    <meta property="fb:admins" content="100000607566694" />
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image:width" content="650" />
+    <meta property="og:image:height" content="366" />
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+    <meta name="copyright" content="dynamics_nfbs" itemprop="dateline" />
+    <meta name="p:domain_verify" content="2057b86bf61e5a346e22a380c6fecf89" />
+    <meta name="robots" content="index, follow" />
+    <meta name="googlebot-news" content="index, follow" />
+    <meta name="googlebot" content="index, follow" />
+    <meta name="kanalid" content="2-3-814-4" />
+    <meta name="articleid" content="6462569" />
+    <meta name="articletype" content="singlepage" />
+    <meta name="articledewasa" content="dewasatidak" />
+    <meta name="articlehoax" content="default" />
+    <meta name="createdate" content="2022/12/15 10:42:48" />
+    <meta name="publishdate" content="2022/12/15 10:47:26" />
+    <meta name="contenttype" content="singlepagenews" />
+    <meta name="platform" content="desktop" />
+    <meta name="hl_nhl_wp" content="nonheadline-0" />
+    <meta name="hl_nhl_kanal" content="nonheadline-0-2-3" />
+    <meta name="videopresent" content="No" />
+
+
+
+    <meta name="idfokus" content="" />
+    <meta name="author" content="IT DYNAMICS NFBS BOGOR" />
+    <meta content="Dynamics 2023 merupakan ajang penguatan dan peningkatan jiwa kepemimpinan pelajar di-Indonesia khususnya di wilayah Jabodatabek, yang nantinya diharapkan dapat mengemban peran vital dilingkungan eksternal maupun internal" itemprop="headline" />
+    <meta name="keywords" content="piala dunia,final piala dunia" itemprop="keywords" />
+    <meta name="thumbnailUrl" content="<?= BASE_URL ?>assets/img/thumbnail.jpg" itemprop="thumbnailUrl" />
+    <meta property="article:author" content="https://www.facebook.com/dynamics_nfbs" itemprop="author" />
+    <meta property="article:publisher" content="https://www.facebook.com/dynamics_nfbs" />
+    <meta name="pubdate" content="2022-12-15T10-47-26Z" itemprop="datePublished" />
+    <meta content="2022-12-15T10-42-48Z" itemprop="dateCreated" />
+    <meta content="<?php echo current_url() ?>" itemprop="url" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@dynamics_nfbs" />
+    <meta name="twitter:site:id" content="@dynamics_nfbs" />
+    <meta name="twitter:creator" content="@dynamics_nfbs" />
+    <meta name="twitter:description" content="Dynamics 2023 merupakan ajang penguatan dan peningkatan jiwa kepemimpinan pelajar di-Indonesia khususnya di wilayah Jabodatabek, yang nantinya diharapkan dapat mengemban peran vital dilingkungan eksternal maupun internal" />
+    <meta name="twitter:image" content="https://akcdn.detik.net.id/api/wm/2022/12/07/pasar-gelap-tiket-piala-dunia-qatar-dijual-10-kali-lipat-dari-harga-resmi_169.jpeg?wid=60&w=650&v=1&t=jpeg" />
+
+
+    <link rel="canonical" href="<?php echo current_url() ?>" />
+    <meta name="dtk:acctype" content="acc-dynamics_nfbs" />
+    <meta name="dtk:kanalid" content="4" />
+    <meta name="dtk:articleid" content="6462569" />
+    <meta name="dtk:articletype" content="singlepage" />
+    <meta name="dtk:articledewasa" content="dewasatidak" />
+    <meta name="dtk:articlehoax" content="default" />
+    <meta name="dtk:createddate" content="2022/12/15 10:42:48" />
+    <meta name="dtk:publishdate" content="2022/12/15 10:47:26" />
+    <meta name="dtk:createddateunix" content="1671075768000" />
+    <meta name="dtk:publishdateunix" content="1671076046000" />
+    <meta name="dtk:contenttype" content="singlepagenews" />
+    <meta name="dtk:platform" content="desktop" />
+    <meta name="dtk:videopresent" content="No" />
+
+
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "BreadcrumbList",
+            "itemListElement": [{
+                    "@type": "ListItem",
+                    "position": 1,
+                    "name": "dynamics_nfbs",
+                    "item": "<?php echo BASE_URL ?>"
+                },
+                {
+                    "@type": "ListItem",
+                    "position": 2,
+                    "name": "Ekonomi Bisnis",
+                    "item": "<?php echo current_url() ?>"
+                }
+
+            ]
+        }
+    </script>
+
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "WebPage",
+            "headline": "Nurul Fikri Leader League, Youth Charity Concert, dan Next Leader Competitions",
+            "url": "<?php echo current_url() ?>",
+            "datePublished": "2022-12-15T10:47:26+07:00",
+            "image": "<?= BASE_URL ?>assets/img/thumbnail.jpg ?>",
+            "thumbnailUrl": "<?= BASE_URL ?>assets/img/thumbnail.jpg ?>"
+        }
+    </script>
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "NewsArticle",
+            "mainEntityOfPage": {
+                "@type": "WebPage",
+                "@id": "<?php echo current_url() ?>"
+            },
+            "headline": "Nurul Fikri Leader League, Youth Charity Concert, dan Next Leader Competitions",
+            "image": {
+                "@type": "ImageObject",
+                "url": "<?= BASE_URL ?>assets/img/thumbnail.jpg ?>"
+            },
+            "datePublished": "2022-12-15T10:47:26+07:00",
+            "dateModified": "2022-12-15T10:47:26+07:00",
+            "author": {
+                "@type": "Person",
+                "name": "Khoirul Mustofa"
+            },
+            "publisher": {
+                "@type": "Organization",
+                "name": "Dynamics NFBS Bogor",
+                "logo": {
+                    "@type": "ImageObject",
+                    "url": "<?= BASE_URL ?>assets/img/logo-dynamics.webp ?>"
+                }
+            },
+            "description": "Dynamics 2023 merupakan ajang penguatan dan peningkatan jiwa kepemimpinan pelajar di-Indonesia khususnya di wilayah Jabodatabek, yang nantinya diharapkan dapat mengemban peran vital dilingkungan eksternal maupun internal"
+        }
+    </script>
+
+    <!-- CSS 
+    ========================= -->
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Exo:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;family=Metal+Mania&amp;display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Naskh+Arabic:wght@400;500&family=Roboto:ital,wght@0,900;1,100;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/slick.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/icofont.min.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/animate.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/nice-select.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/preloader.css">
+
+    <!-- Timeline CSS -->  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/style.css">
+    <?= $this->renderSection('loadCss') ?>
+</head>
+
+<body class="body__bg" data-bgimg="<?= BASE_URL ?>assets/img/bg/body-bg.webp">
+
+    <!--header area start-->
+    <header class="header_section header_transparent sticky-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="main_header d-flex justify-content-between align-items-center">
+                        <div class="header_logo">
+                            <a class="sticky_none" href="#"><img aria-label="logo" width="215" height="79" src="<?= BASE_URL ?>assets/img/logo-dynamics.webp" alt="logo-dynamics.webp"></a>
+                        </div>
+                        <!--main menu start-->
+                        <div class="main_menu d-none d-lg-block">
+                            <nav>
+                                <ul class="d-flex magic-hover magic-hover__square">
+                                    <li><a href="<?php echo BASE_URL ?>">HOME</a></li>
+                                    <li><a href="<?php echo BASE_URL ?>#about">ABOUT</a></li>
+                                    <li><a href="<?php echo BASE_URL . 'competition' ?>">COMPETITION</a></li>
+                                    <li><a href="<?php echo BASE_URL . 'homestay' ?>">HOMESTAY</a></li>
+                                    <li><a href="<?php echo BASE_URL . 'contact' ?>">CONTACT</a></li>
+                                    <!-- <li><a href="<?php echo BASE_URL . 'nfll' ?>">NFLL</a></li> -->
+                                </ul>
+                            </nav>
+                        </div>
+                        <!--main menu end-->
+                        <div class="header_right_sidebar d-flex align-items-center">
+                            <div class="sing_up_btn">
+                                <a class="btn btn-link magic-hover magic-hover__square" href="<?php echo BASE_URL . "competition" ?>">JOIN NOW <img width="15" height="15" src="assets/img/icon/arrrow-icon2.webp" alt=""> </a>
+                            </div>
+                            <div class="canvas_open">
+                                <button type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu"><i class="icofont-navigation-menu"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!--header area end-->
+
+    <!--offcanvas menu area start-->
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasMenu">
+        <div class="offcanvas-header justify-content-end">
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <ul class="offcanvas_main_menu">
+                <li class="menu-item-has-children"><a href="<?php echo BASE_URL ?>">HOME</a></li>
+                <li class="menu-item-has-children"><a href="<?php echo BASE_URL ?>#about">ABOUT</a></li>
+                <li class="menu-item-has-children"><a href="<?php echo BASE_URL . 'competition' ?>">COMPETITION</a></li>
+                <li class="menu-item-has-children"><a href="#">HOMESTAY</a></li>
+                <li class="menu-item-has-children"><a href="<?php echo BASE_URL . 'contact' ?>">CONTACT</a></li>
+                <!-- <li class="menu-item-has-children"><a href="<?php echo BASE_URL . 'nfll' ?>">NFLL</a></li> -->
+            </ul>
+        </div>
+    </div>
+    <!--offcanvas menu area end-->
+
+    <!-- page wrapper start -->
+    <?= $this->renderSection('content') ?>
+    <!-- page wrapper end -->
+
+    <!--footer area start-->
+
+    <footer class="footer_widgets">
+        <div class="main_footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="main_footer_inner d-flex">
+                            <div class="footer_widget_list contact">
+                                <div class="footer_logo">
+                                    <a href="#"><img aria-label="logo" width="215" height="79" src="assets/img/logo-nf-putih.webp" alt=""></a>
+                                </div>
+                                <div class="footer_contact_desc">
+                                    <p>Educating The Future Islamic Leaders</p>
+                                </div>
+                                <div class="footer_social">
+                                    <ul class="d-flex">
+                                        <li><a aria-label="facebook" class="facebook" href="https://www.facebook.com/nurulfikriboardingschoolbogor"><i class="icofont-facebook"></i></a></li>
+                                        <li><a aria-label="youtube" class="youtube" href="https://www.youtube.com/c/NurulFikriBoardingSchoolBogor"><i class="icofont-youtube-play"></i></a></li>
+                                        <li><a aria-label="twitter" class="twitter" href="https://twitter.com/nfbsbogor/"><i class="icofont-twitter"></i></a></li>
+                                        <li><a aria-label="instagram" class="youtube" href="https://instagram.com/dynamics.nfbsbogor?igshid=YmMyMTA2M2Y="><i class="icofont-instagram"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="footer_widget_list contact">
+                                <h3>Contact</h3>
+                                <div class="footer_contact_info">
+                                    <div class="footer_contact_info_list">
+                                        <span>Location:</span>
+                                        <p>Nurul Fikri Boarding School Bogor, Jl Jami, Sukaluyu, Tamansari, Bogor, Jawa Barat, 16610</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="footer_widget_list contact">
+                                <h3>SITE LINK</h3>
+                                <div class="footer_menu">
+                                    <ul>
+                                        <li><a href="https://www.nfbs-bogor.sch.id/"> Website NFBS Bogor </a></li>
+                                        <li><a href="#">BSS NFBS Bogor</a></li>
+                                        <li><a href="#">E-Library NFBS Bogor</a></li>
+                                        <li><a href="#">CBT NFBS Bogor</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer_bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="footer_bottom_inner d-flex justify-content-between">
+                            <div class="copyright_right">
+                                <p> © 2023 MADE WITH <i class="icofont-heart"></i> by <a href="https://hasthemes.com/">IT DYNAMICS</a></p>
+                            </div>
+                            <div class="footer_bottom_link_menu">
+                                <ul class="d-flex">
+                                    <li><a href="#">Terms & Condition </a></li>
+                                    <li><a href="#">Privacy Policy </a></li>
+                                </ul>
+                            </div>
+
+                            <div class="scroll__top_icon">
+                                <a id="scroll-top" href="#"><img aria-label="scroll-top" width="46" height="40" src="assets/img/icon/scroll-top.webp" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    </div>
+
+    <!--footer area end-->
+
+
+    <!-- JS
+============================================ -->
+    <!--modernizr min js here-->
+    <script src="<?= BASE_URL ?>assets/js/vendor/modernizr-3.7.1.min.js"></script>
+
+    <!-- Vendor JS -->
+    <script src="<?= BASE_URL ?>assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="<?= BASE_URL ?>assets/js/vendor/jquery-migrate-3.3.2.min.js"></script>
+    <script src="<?= BASE_URL ?>assets/js/vendor/popper.js"></script>
+    <script src="<?= BASE_URL ?>assets/js/vendor/bootstrap.min.js"></script>
+    <script src="<?= BASE_URL ?>assets/js/slick.min.js"></script>
+    <script src="<?= BASE_URL ?>assets/js/wow.min.js"></script>
+    <script src="<?= BASE_URL ?>assets/js/jquery.nice-select.js"></script>
+    <script src="<?= BASE_URL ?>assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="<?= BASE_URL ?>assets/js/jquery.counterup.min.js"></script>
+    <script src="<?= BASE_URL ?>assets/js/jquery-waypoints.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script type="text/javascript" src="https://res.cloudinary.com/veseylab/raw/upload/v1636192990/magicmouse/magic_mouse-1.2.1.cdn.min.js"></script>
+
+    <!-- Main JS -->
+    <script src="<?= BASE_URL ?>assets/js/main.js"></script>
+    <?= $this->renderSection('loadJs') ?>
+    <script>
+        options = {
+            "cursorOuter": "circle-basic",
+            "hoverEffect": "pointer-overlay",
+            "hoverItemMove": false,
+            "defaultCursor": false,
+            "outerWidth": 40,
+            "outerHeight": 40
+        };
+        magicMouse(options);
+    </script>
+</body>
+
+</html>
